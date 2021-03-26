7428527833686b85ae748449458e75d2193fe1f7
Ricardo Moreira <70721082+Ricardosssss@users.noreply.github.com>
2021-03-26 21:45:11 +0000

    Update SpamBot.py

--- SpamBot.py
+++ SpamBot.py
@@ -1,29 +1,57 @@
 from time import sleep
 import pyautogui
 
-print('https://github.com/Ricardosssss')
-
 print('''
-,_._._._._._._._|________________________________________________________
-|_|_|_|_|_|_|_|_|_______________________________________________________/
-                !
+https://www.instagram.com/_.ricardosssss._/
 
-███████╗██████╗  █████╗ ███╗   ███╗
-██╔════╝██╔══██╗██╔══██╗████╗ ████║
-███████╗██████╔╝███████║██╔████╔██║
-╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║
-███████║██║     ██║  ██║██║ ╚═╝ ██║
-╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝
+ ,_._._._._._._._|________________________________________________________
+ |_|_|_|_|_|_|_|_|_______________________________________________________/
+                 !
+
+ ███████╗██████╗  █████╗ ███╗   ███╗
+ ██╔════╝██╔══██╗██╔══██╗████╗ ████║
+ ███████╗██████╔╝███████║██╔████╔██║
+ ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║
+ ███████║██║     ██║  ██║██║ ╚═╝ ██║
+ ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝
 ''')
 
-x = input('Message: ')
+print('''
+[ 1 ]Spam infinito
+[ 2 ]Spam contado
+''')
 
-print('It will start in')
-
-for n in range(2, -1, -1):
-    print(f'[ {n + 1} ]')
-    sleep(1)
+e = int(input('Opção: '))
 
 while 1:
-    pyautogui.write(x)
-    pyautogui.press('enter')
+    if e >= 3:
+        print('''
+[ 1 ]Spam infinito
+[ 2 ]Spam contado
+    ''')
+        e = int(input('Opção: '))
+    else:
+        break
+
+x = input('Mensagem: ')
+
+if e == 1: 
+    print('Vai começar em')
+    for n in range(2, -1, -1):
+        print(f'[ {n + 1} ]')
+        sleep(1)
+    
+    while True:
+        pyautogui.write(x)
+        pyautogui.press('enter')
+
+elif e == 2:
+    q = int(input('Quantas vezes repetir? '))
+    print('Vai começar em')
+    for n in range(2, -1, -1):
+        print(f'[ {n + 1} ]')
+        sleep(1)
+    
+    for v in range(0, q):
+        pyautogui.write(x)
+        pyautogui.press('enter')
